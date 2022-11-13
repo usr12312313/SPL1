78dc75e4c6963156d5b4edd9bd62ed029c0af94e
yuval1991999 <yuval1991999@gmail.com>
2022-11-13 21:15:51 +0200

    Update Agent.cpp

--- SPL1/src/Agent.cpp
+++ SPL1/src/Agent.cpp
@@ -17,7 +17,12 @@ int Agent::getPartyId() const
 
 void Agent::step(Simulation &sim)
 {
-    // TODO: implement this method
-    // offer prefered party to join, if party is waiting change its state to collecting offers
-    //
+    /*
+     TODO: implement this method
+     offer prefered party to join, if party is waiting change its state to collecting offers
+     notice: there is only one agent for each party (The total number of agents in the simulation equals the- 
+     number of parties in the graph belonging to some coalition)
+     an agent can only offer to a party which his coalition hasnt offered yet, we should implement a way to check this- 
+     (The party did not already receive an offer from any agent from the coalition to which the agent belongs.)
+    */
 }
